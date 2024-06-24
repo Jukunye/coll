010b0929e6a0edec6b23c52703aa2109a14e21da
Jukunye <jukunyes@gmail.com>
2024-06-24 08:19:55 +0300

    CreateUserDto with firstName, lastName, email, and password validation using class-validator

---
+++ backend/src/users/dto/create-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+}
