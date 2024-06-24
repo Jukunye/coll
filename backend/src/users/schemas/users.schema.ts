7d858df5bbbf09e5d73fba1bdeedf1abbbce8f6c
Jukunye <jukunyes@gmail.com>
2024-06-24 08:20:43 +0300

    Added UserSchema and User model with email, firstName, lastName, and password properties using @nestjs/mongoose and mongoose

---
+++ backend/src/users/schemas/users.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export type UserDocument = User & Document<Types.ObjectId>;
+
+@Schema()
+export class User {
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  firstName: string;
+
+  @Prop({ required: true })
+  lastName: string;
+
+  @Prop({ required: true })
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
