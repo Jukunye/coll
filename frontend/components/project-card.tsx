3c8f511c49bc820c18a86ba0d47c6800d066b776
Jukunye <jukunyes@gmail.com>
2024-06-29 22:56:22 +0300

    style: :lipstick: add styling to project card

--- frontend/components/project-card.tsx
+++ frontend/components/project-card.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from './ui/button';
 
 type Props = {
   owner: string;
@@ -20,14 +21,31 @@ const ProjectCard: React.FC<Props> = ({
   language,
 }) => {
   return (
-    <div>
-      <p>{owner}</p>
-      <p>{start}</p>
-      <img src={image} alt={title} />
-      <h2>{title}</h2>
-      <p>{description}</p>
-      {level && <p>Level: {level}</p>}
-      {language && <p>Language: {language}</p>}
+    <div className="border rounded-md max-w-xs">
+      <div className="flex justify-between text-sm px-6 py-3">
+        <p>{owner}</p>
+        <p className="text-xs text-slate-400">{start}</p>
+      </div>
+      <img
+        src={image}
+        alt={title}
+        className="max-w-xs h-auto object-cover bg-slate-100"
+      />
+      <div className="px-6 py-3 text-sm">
+        <div className="flex justify-between my-2">
+          {level && (
+            <p className="bg-slate-50 text-slate-600 py-1 px-3 rounded-full">
+              {level}
+            </p>
+          )}
+          {language && <p className="text-slate-600">{language}</p>}
+        </div>
+        <h2 className="text-lg font-semibold">{title}</h2>
+        <p className="mt-2 text-slate-500">{description}</p>
+      </div>
+      <div className="flex justify-end px-6 pb-4">
+        <Button variant="outline">Join</Button>
+      </div>
     </div>
   );
 };
