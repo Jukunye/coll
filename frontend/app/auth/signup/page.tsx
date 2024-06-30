b824eccc0e2cccfc0ea28134c8e7f649035ade70
Jukunye <jukunyes@gmail.com>
2024-06-30 08:17:20 +0300

    Sign up page

--- frontend/app/auth/signup/page.tsx
+++ frontend/app/auth/signup/page.tsx
@@ -1,7 +1,12 @@
+import RegistrationForm from '@/components/forms/registration';
 import React from 'react';
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="h-screen w-full flex items-center justify-center px-4">
+      <RegistrationForm />
+    </div>
+  );
 }
 
 export default SignUp;
