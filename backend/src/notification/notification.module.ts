6b2c2f1c206a526fc83c4ad166baca914c8f0a91
Jukunye <jukunyes@gmail.com>
2024-07-06 22:22:11 +0300

    Create a notification schema and connect to MongoDB

--- backend/src/notification/notification.module.ts
+++ backend/src/notification/notification.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Notification,
+  NotificationSchema
+} from './schemas/notification.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Notification.name, schema: NotificationSchema }
+    ])
+  ],
   providers: [NotificationService],
   controllers: [NotificationController]
 })
---
+++ backend/src/notification/schemas/notification.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+
+@Schema()
+export class Notification extends Document {
+  @Prop({ required: true, type: MongooseSchema.Types.ObjectId, ref: 'User' })
+  user: MongooseSchema.Types.ObjectId;
+
+  @Prop({ required: true })
+  type: string; // e.g., 'comment', 'join', 'new_project'
+
+  @Prop({ required: true })
+  message: string;
+
+  @Prop({ required: true })
+  link: string; // URL to the relevant page
+
+  @Prop({ default: false })
+  read: boolean;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+}
+
+export const NotificationSchema = SchemaFactory.createForClass(Notification);
